3a883af63312b641ce49a0a693510eb31f87daaa
Hafeed10 <hafeex@gamil.com>
2024-06-10 22:12:44 +0530

    port 3.0

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='flex'>
+            aaaaaaaaaaaaaaaaaaaa
+        </div >
+    );
+}
+
+export default NavBar;
