4463ce41a5c622e7e34abafa32da7d6a5a6354b8
Hafeed10 <hafeex@gamil.com>
2024-06-18 20:00:27 +0530

    add pege

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '../Container/Container'; // Corrected import statement
+
+function Home() {
+    return (
+        <Container>
+            <h1>Home Page</h1>
+            <p>lorem10 ipusm lam lam </p>
+        </Container>
+    );
+}
+
+export default Home;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../Home/Home';  // Adjusted relative path to Home component
+import About from '../About/About'
 const Layout = () => {
     return (
         <div>
-            <h1> Layout</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
